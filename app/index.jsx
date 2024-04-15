f81b0ed07126f86891a3cfa38097b1e4d4dc6753
giannis198 <john_nik@windowslive.com>
2024-04-15 11:56:24 +0300

    feat:tabs created

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
+      <Link href="/home" className="text-red-300">
+        Go to Profile
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
